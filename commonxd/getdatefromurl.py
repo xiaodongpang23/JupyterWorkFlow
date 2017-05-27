82bed566a48a62accc6b715729cadab46fc4f94e
Xiaodong's Ubuntu <xiaodong0119@gmail.com>
2017-05-26 23:39:18 -0400

    use Python strftime reference to speedup converting data into date format

--- commonxd/getdatefromurl.py
+++ commonxd/getdatefromurl.py
@@ -24,7 +24,9 @@ def get_data_fromurl(filename='Fremont.csv', url=Data_URL,
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
+    #data = pd.read_csv(filename, index_col='Date', parse_dates=True)
     data = pd.read_csv(filename, index_col='Date')
+    # below is to speed up parse_dates by specifying the format of date in the data file, for more format see "Python strftime reference" online
 
     try:
         data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
